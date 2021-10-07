6660ea20af99b03484051ad737abd91060c37357
Asumji <64544378+Asumji@users.noreply.github.com>
2021-10-07 18:57:11 +0200

    Update Main.java

--- hardcorerevive/src/main/java/me/asumji/hardcorerevive/Main.java
+++ hardcorerevive/src/main/java/me/asumji/hardcorerevive/Main.java
@@ -1,8 +1,10 @@
 package me.asumji.hardcorerevive;
 
 import me.asumji.hardcorerevive.Files.DataManager;
+import me.asumji.hardcorerevive.Files.EventListener;
 import me.asumji.hardcorerevive.commands.reviveCommand;
 import me.asumji.hardcorerevive.commands.revivePriceCommand;
+import me.asumji.hardcorerevive.commands.reviveToggleCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -12,8 +14,13 @@ public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+
+        Main main = this;
+
         getServer().getPluginCommand("revive").setExecutor(new reviveCommand(this));
         getServer().getPluginCommand("reviveprice").setExecutor(new revivePriceCommand(this));
+        getServer().getPluginCommand("revivetoggle").setExecutor(new reviveToggleCommand(this));
+        getServer().getPluginManager().registerEvents(new EventListener(main), this);
         this.data = new DataManager(this);
         this.data.loadConfiguration();
     }
@@ -22,4 +29,5 @@ public class Main extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
     }
+
 }
