575c51171b0d6ba6ab8f73033b90d41dd93891c6
Asumji <64544378+Asumji@users.noreply.github.com>
2021-12-22 15:35:11 +0100

    fixed a few minor bugs

--- hardcorerevive/src/main/java/me/asumji/hardcorerevive/commands/reviveCommand.java
+++ hardcorerevive/src/main/java/me/asumji/hardcorerevive/commands/reviveCommand.java
@@ -6,13 +6,9 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class reviveCommand implements CommandExecutor {
     private final  Main main;
@@ -20,52 +16,15 @@ public class reviveCommand implements CommandExecutor {
     public reviveCommand(Main main) {
         this.main = main;
     }
-
-    public static int removeItems(Inventory inventory, Material type, int amount) {
-
-        if(type == null || inventory == null)
-            return -1;
-        if (amount <= 0)
-            return -1;
-
-        if (amount == Integer.MAX_VALUE) {
-            inventory.remove(type);
-            return 0;
-        }
-
-        HashMap<Integer, ItemStack> retVal = inventory.removeItem(new ItemStack(type,amount));
-
-        int notRemoved = 0;
-        for(ItemStack item: retVal.values()) {
-            notRemoved+=item.getAmount();
-        }
-        return notRemoved;
-    }
-
-    public static boolean containsAtLeast(Player p, Material m, Integer amount) {
-        AtomicReference<Integer> has = new AtomicReference<>(0);
-
-        Arrays.stream(p.getInventory().getContents()).forEach(itemStack -> {
-            if (itemStack != null && itemStack.getType() != null && itemStack.getType().equals(m))
-                has.updateAndGet(v -> v + itemStack.getAmount());
-        });
-
-        if (has.get() >= amount)
-            return true;
-
-        return false;
-    }
-
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
 
         int amount = this.main.getConfig().getItemStack("revive.price").getAmount();
-        main.getLogger().info(String.valueOf(amount));
-        Material item = this.main.getConfig().getItemStack("revive.price").getType();
+        ItemStack item = this.main.getConfig().getItemStack("revive.price");
 
         if (sender instanceof Player) {
-            if (main.getConfig().get("revive.method") == "command") {
+            if (((String) main.getConfig().get("revive.method")).equalsIgnoreCase("command")) {
                 Player player = (Player) sender;
                 if (args.length > 0) {
                     if (player.getGameMode() == GameMode.SPECTATOR) {
@@ -73,11 +32,10 @@ public class reviveCommand implements CommandExecutor {
                         return true;
                     } else {
                         Player target = Bukkit.getPlayerExact(args[0]);
-                        Inventory inv = player.getInventory();
                         if (target instanceof Player) {
                             if (target != player) {
                                 if (target.getGameMode() == GameMode.SPECTATOR) {
-                                    if (containsAtLeast(player, item, amount)) {
+                                    if (player.getInventory().containsAtLeast(item, amount)) {
                                         target.setGameMode(GameMode.SURVIVAL);
                                         List<World> worlds = this.main.getServer().getWorlds();
 
@@ -86,15 +44,15 @@ public class reviveCommand implements CommandExecutor {
                                         Location location = this.main.getServer().getWorld(world).getSpawnLocation();
                                         target.teleport(location);
 
-                                        removeItems(inv, item, amount);
+                                        player.getInventory().removeItem(new ItemStack(item.getType(),amount));
                                         player.sendMessage(ChatColor.GREEN + "You've revived " + args[0]);
                                         target.sendMessage(ChatColor.GREEN + "You've been revived by " + player.getDisplayName());
                                         return true;
                                     } else {
-                                        String item1 = String.valueOf(item);
+                                        String item1 = String.valueOf(item.getType());
                                         item1 = item1.replace("_", " ");
                                         item1 = item1.toLowerCase();
-                                        player.sendMessage(ChatColor.RED + "You need at least " + amount + " " + item1 + " to revive someone!");
+                                        player.sendMessage(ChatColor.RED + "You need at least " + amount + " " + item1 + " to revive someone!\nMake sure the item isn't in any extra slot (Off-hand or similar) but in the inventory.");
                                         return true;
                                     }
                                 } else {
