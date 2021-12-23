15bce7f061760d2844ee6769211bdae02d3c85a8
Asumji <64544378+Asumji@users.noreply.github.com>
2021-12-23 18:26:38 +0100

    spawn at death location

--- hardcorerevive/src/main/java/me/asumji/hardcorerevive/Files/EventListener.java
+++ hardcorerevive/src/main/java/me/asumji/hardcorerevive/Files/EventListener.java
@@ -1,12 +1,12 @@
 package me.asumji.hardcorerevive.Files;
 import me.asumji.hardcorerevive.Main;
 import org.bukkit.*;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDamageEvent;
-
-import java.util.List;
+import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class EventListener implements Listener {
 
@@ -26,13 +26,22 @@ public class EventListener implements Listener {
                     for (Player p : Bukkit.getOnlinePlayers()) {
                         if (p.getGameMode() == GameMode.SPECTATOR) {
 
-                            List<World> worlds = this.main.getServer().getWorlds();
+                            if (((String) main.getConfig().get("revive.spawn")).equalsIgnoreCase("spawn")) {
+                                String world = p.getLocation().getWorld().getName();
 
-                            String world = worlds.get(0).getName();
+                                Location location = this.main.getServer().getWorld(world).getSpawnLocation();
+                                p.teleport(location);
+                            } else if (((String) main.getConfig().get("revive.spawn")).equalsIgnoreCase("death")) {
+                                ConfigurationSection section = main.getConfig().getConfigurationSection(p.getUniqueId().toString());
+                                if (section.getString("cause").equalsIgnoreCase("nonReviviable")) {
+                                    String world = p.getLocation().getWorld().getName();
 
-                            Location location = this.main.getServer().getWorld(world).getSpawnLocation();
-                            p.teleport(location);
-
+                                    Location location = this.main.getServer().getWorld(world).getSpawnLocation();
+                                    p.teleport(location);
+                                } else {
+                                    p.teleport(new Location(p.getLocation().getWorld(), section.getDouble("location.x"), section.getDouble("location.y"), section.getDouble("location.z")));
+                                }
+                            }
                             p.setGameMode(GameMode.SURVIVAL);
                             p.sendMessage(ChatColor.GREEN + "You've been revived!");
                             Bukkit.getServer().broadcastMessage(ChatColor.GREEN + p.getName() + " has been revived through a ritual!");
@@ -42,5 +51,27 @@ public class EventListener implements Listener {
                 }
             }
         }
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent e) {
+        Player p = e.getEntity();
+        if (!main.getConfig().contains(p.getUniqueId().toString())) {
+            main.getConfig().createSection(p.getUniqueId().toString());
+        }
+        ConfigurationSection section = main.getConfig().getConfigurationSection(p.getUniqueId().toString());
+        section.set("location.x", Integer.valueOf(p.getLocation().getBlockX()));
+        section.set("location.y", Integer.valueOf(p.getLocation().getBlockY()));
+        section.set("location.z", Integer.valueOf(p.getLocation().getBlockZ()));
+        EntityDamageEvent.DamageCause pCause = p.getLastDamageCause().getCause();
+        EntityDamageEvent.DamageCause lavaCause = EntityDamageEvent.DamageCause.LAVA;
+        EntityDamageEvent.DamageCause voidCause = EntityDamageEvent.DamageCause.VOID;
+        EntityDamageEvent.DamageCause suffocCause = EntityDamageEvent.DamageCause.SUFFOCATION;
+        if (pCause.equals(lavaCause) || pCause.equals(voidCause) || pCause.equals(suffocCause)) {
+            section.set("cause", "nonReviviable");
+        } else {
+            section.set("cause", "reviviable");
+        }
+        main.saveConfig();
     }
 }
